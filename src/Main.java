982e3aa0dc16b8e2064d28f0a3216417c4051c9c
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2023-06-21 13:31:05 +0100

    신고 결과 받기

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        class Solution {
+            public int[] solution(String[] id_list, String[] report, int k) {
+                int[] answer = new int[id_list.length];
+                Map<String, HashSet<String>> reporter = new HashMap<>();
+                Map<String, Integer> count = new HashMap<>();
 
+                for(int i = 0; i < id_list.length; i++){
+                    String name = id_list[i];
+                    reporter.put(name, new HashSet<>()); //신고 당한 사람 이름을 넣는다.
+                    count.put(name,i);
+                }
+
+                for(String s : report){
+                    String[] str = s.split(" ");
+                    String a = str[0];
+                    String b = str[1];
+                    reporter.get(b).add(a); //신고 당한 사람 가져와서 신고자 이름을 넣는다.
+                }
+
+                for(int i = 0; i<id_list.length; i++){
+                    HashSet<String> mail = reporter.get(id_list[i]); //신고당한 사람을 키로 신고자 값을 가져온다.
+                    if(mail.size() >= k ){  //mail.size() == 신고자의 명수
+                        for(String name: mail){
+                            answer[count.get(name)]++;
+                        }
+                    }
+                }
+                return answer;
+            }
+        }
     }
-
 }
