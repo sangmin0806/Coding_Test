6a39135de885cdb0bd1ad6ec4ea414f05732ee96
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2023-06-21 14:44:24 +0100

    성공

--- src/PhoneCatMon.java
+++ src/PhoneCatMon.java
@@ -1,5 +1,22 @@
+import java.util.*;
 public class PhoneCatMon {
     public static void main(String[] args) {
+        class Solution {
+            public int solution(int[] nums) {
+                int answer = 0;
+                int length = nums.length;
+                HashSet<Integer> type = new HashSet<>();
+                for(int i = 0; i< length; i++){
+                    type.add(nums[i]);
+                }
+                int count = type.size();
+                if((length/2)<count)
+                    answer = length/2;
+                else
+                    answer = count;
 
+                return answer;
+            }
+        }
     }
 }
