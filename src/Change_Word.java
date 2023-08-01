6f3b1a1ee85c9ef9e0581bee1b0697f9eb6ebb48
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2023-08-01 20:15:27 +0900

    success

---
+++ src/Change_Word.java
@@ -0,0 +1,61 @@
+public class Change_Word {
+    static boolean[] visited;
+    static int answer = 0;
+    static int result = 0;
+    static boolean first = true;
+
+    public int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+        boolean have = false;
+        for(int i = 0; i<words.length; i++){
+            if(words[i].equals(target)){
+                have = true;
+            }
+        }
+        if(!have)
+            return result;
+        dfs(begin,target,words);
+        return result;
+
+    }
+    public void dfs(String begin, String target, String[] words){
+        int targetcount = 0;
+        for(int j = 0; j<begin.length(); j++){
+            if(begin.charAt(j)==target.charAt(j)){
+                targetcount++;
+            }
+        }
+        if(targetcount == (begin.length()-1)&&first){
+            answer++;
+            first = false;
+            result = answer;
+            return;
+        }
+
+        for(int i = 0; i<words.length; i++){
+            if(visited[i])
+                continue;
+            int count = 0;
+            for(int j = 0; j<begin.length(); j++){
+                if(begin.charAt(j)==words[i].charAt(j)){
+                    count++;
+                }
+            }
+            if(count == (begin.length()-1)){
+                begin = words[i];
+                visited[i] = true;
+                answer++;
+                dfs(begin,target,words);
+            }
+
+        }
+    }
+    public static void main(String[] args){
+        Change_Word sol = new Change_Word();
+        String begin = "hit";
+        String target = "cog";
+        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+        int result = sol.solution(begin,target,words);
+        System.out.println(result);
+    }
+}
