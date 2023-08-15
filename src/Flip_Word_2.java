ae578fce51037a66848ba3ac067cc2596cd41d69
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2023-08-15 15:27:27 +0900

    fail...

---
+++ src/Flip_Word_2.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Flip_Word_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        boolean bracket = false;
+        Stack<String> stack = new Stack<>();
+        for(int i =0; i<str.length(); i++){
+            if(str.charAt(i)=='<'){
+                while(!stack.isEmpty()) {
+                    sb.append(stack.pop());
+                }
+                bracket = true;
+            }
+            else if(str.charAt(i)=='>'){
+                bracket = false;
+                sb.append(">");
+                continue;
+            }
+            if(!bracket){
+                if(str.charAt(i)==' '){
+                    while(!stack.isEmpty()) {
+                        sb.append(stack.pop());
+                    }
+                    sb.append(' ');
+                }
+                else{
+                    stack.add(String.valueOf(str.charAt(i)));
+                }
+                if(i == str.length()-1){
+                    while(!stack.isEmpty()) {
+                        sb.append(stack.pop());
+                    }
+                }
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
