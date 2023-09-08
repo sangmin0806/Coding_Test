f979ef58187ca0b497d15b3502189deac035d56d
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2023-09-08 13:36:09 +0900

    success

---
+++ src/Frequency_Sort.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Frequency_Sort {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        List<Integer> list = new ArrayList<>();
+        HashMap<Integer,Integer> map = new LinkedHashMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int arr = Integer.parseInt(st.nextToken());
+            list.add(arr) ;
+            map.put(arr,map.getOrDefault(arr,0)+1);
+        }
+        ArrayList<Integer> keylist = new ArrayList<>(map.keySet());
+        Collections.sort(keylist, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return Integer.compare(map.get(b), map.get(a));
+            }
+        });
+        for (int i = 0; i < keylist.size(); i++) {
+            for (int j = 0; j < map.get(keylist.get(i)); j++) {
+                System.out.print(keylist.get(i)+" ");
+            }
+        }
+
+
+    }
+}
