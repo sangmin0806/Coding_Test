7174635c96ce5b33bc409b50473942574a9273d1
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2023-11-08 23:07:59 +0900

    [D3] Title: N-Queen, Time: 3,277 ms, Memory: 23,220 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -0,0 +1,76 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	static int N;
+    static int[][] map;
+    static int answer = 0;
+    public static void main(String args[]) throws Exception
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            N = sc.nextInt();
+            answer = 0;
+            map = new int[N][N];
+            dfs(0,0,0);
+            System.out.println("#" +test_case+" "+answer);
+        }
+    }
+    public static void dfs(int row, int col,int count){
+
+        if(count==N){
+//            for (int i = 0; i < N; i++) {
+//                for (int j = 0; j < N; j++) {
+//                    System.out.print(map[i][j]+" ");
+//                }
+//                System.out.println();
+//            }
+//            System.out.println();
+            answer++;
+        }
+        for (int i = row; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(check(i,j)){
+                    map[i][j] = 1;
+                    dfs(i+1,j,count+1);
+                    map[i][j] = 0;
+                }
+            }
+        }
+
+    }
+    public static boolean check(int row, int col){
+        boolean flag = false;
+        for (int i = 0; i < N; i++) {
+            if(map[row][i]!=0)flag = true;
+        }
+        for (int i = 0; i < N; i++) {
+            if(map[i][col]!=0) flag = true;
+        }
+        for (int i = 0; i < N; i++) {
+            if(row+i<N&&col+i<N){
+                if(map[row+i][col+i]!=0) flag = true;
+            }
+            if(row-i>=0&&col+i<N){
+                if(map[row-i][col+i]!=0) flag = true;
+            }
+            if(row+i<N&&col-i>=0){
+                if(map[row+i][col-i]!=0) flag = true;
+            }
+            if(row-i>=0&&col-i>=0){
+                if(map[row-i][col-i]!=0) flag = true;
+            }
+        }
+        if(!flag){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ SWEA/D3/2806. N－Queen/README.md
@@ -0,0 +1,15 @@
+# [D3] N-Queen - 2806 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV7GKs06AU0DFAXB) 
+
+### 성능 요약
+
+메모리: 23,220 KB, 시간: 3,277 ms, 코드길이: 2,001 Bytes
+
+### 제출 일자
+
+2023-11-08 23:07
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
