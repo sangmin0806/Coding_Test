456db69250d28e079561b0c5b6d22afa3cf36a65
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2023-11-28 19:18:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 192 ms, Memory: 53824 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -9,29 +9,26 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N+1][2];
+        int[] W = new int[N+1];
+        int[] V = new int[N+1];
         for (int i = 1; i < N+1; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            W[i] = Integer.parseInt(st.nextToken());
+            V[i] = Integer.parseInt(st.nextToken());
         }
-        int[][] dp = new int[K+1][N+1];
-        for (int i = 1; i < K+1; i++) {
-            for (int j = 1; j < N+1; j++) {
-                if(arr[j][0]<=i){
-                    dp[i][j] = Math.max(dp[i-arr[j][0]][j-1]+arr[j][1],Math.max(dp[i][j-1],arr[j][1]));
+        int[][] dp = new int[N+1][K+1];//물건의 인덱스별, 가치별 dp. 같은 dp여도, 다른 인덱스 일수도 있어서
+        for (int i = 1; i < N+1; i++) {
+            for (int j = 1; j < K+1; j++) {
+                if(j-W[i]>=0){
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-W[i]]+V[i]);
                 }
                 else {
-                    dp[i][j] = dp[i][j-1];
+                    dp[i][j] = dp[i-1][j];
                 }
+
             }
         }
-        System.out.println(dp[K][N]);
-//        for (int i = 1; i < N+1; i++) {
-//            for (int j = 1; j < K+1; j++) {
-//                System.out.print(dp[j][i]+" ");
-//            }
-//            System.out.println();
-//        }
+        System.out.println(dp[N][K]);
+        
     }
 }
