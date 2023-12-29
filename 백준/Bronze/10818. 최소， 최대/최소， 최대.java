b3aa703c71623b31dc2a4434c9e0bce2c7cfbec7
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2023-12-29 22:06:21 +0900

    [Bronze III] Title: 최소, 최대, Time: 1736 ms, Memory: 311324 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < num; i++) {
+            int a = sc.nextInt();
+            if(max<a) max = a;
+            if(min>a) min = a;
+        }
+//        int[] list = new int[num];
+//        for (int i = 0; i < num; i++) {
+//            list[i] = sc.nextInt();
+//        }
+//        for (int i = 0; i < num; i++) {
+//            if(max<list[i]) max = list[i];
+//            if(min>list[i]) min = list[i];
+//        }
+        System.out.println(min+" "+max);
+    }
+}
