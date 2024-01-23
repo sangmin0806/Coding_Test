3123d1a18f45a5788426aa25cb9e0daa096d35cc
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2024-01-23 21:50:08 +0900

    [Gold V] Title: 집합의 표현, Time: 1388 ms, Memory: 56984 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        parent = new int[N+1];
+        for (int i = 0; i < N+1; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int function = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if(function==0){
+                Union(a,b);
+            }else {
+                find(a,b);
+            }
+        }
+    }
+    public static int getParent(int a){
+        if(parent[a]==a) return parent[a];
+
+        return parent[a] = getParent(parent[a]);
+
+    }
+    public static void Union(int a,int b){
+        a = getParent(a);
+        b = getParent(b);
+        if(a<b){
+            parent[b] = a;
+        }else {
+            parent[a] = b;
+        }
+    }
+    public static void find(int a, int b){
+        a = getParent(a);
+        b = getParent(b);
+        if(a==b) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
