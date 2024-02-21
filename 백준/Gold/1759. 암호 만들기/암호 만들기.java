d56eb225a896d41f01befb5061e4b4683ad3165e
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2024-02-21 21:59:09 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,17 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
 import java.util.StringTokenizer;
 
 public class Main {
     static int L,C;
     static char[] arr;
-    static StringBuilder sbanswer = new StringBuilder();
-    static LinkedHashSet<String> hashSet = new LinkedHashSet<>();
+    static int mocount;
+    static int jacount;
+    static StringBuilder sb = new StringBuilder();
     static char[] answer;
     static boolean[] visited;
-    static char[] temp ;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,43 +21,36 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         arr =new char[C];
         answer = new char[L];
-        temp = new char[L];
+
         visited = new boolean[C];
         for (int i = 0; i < C; i++) {
             arr[i] = st.nextToken().charAt(0);
         }
         Arrays.sort(arr);
-        recur(0);
-        for(String str :hashSet) {
-            sbanswer.append(str).append("\n");
-        }
-        System.out.println(sbanswer);
+        recur(0,0);
+        System.out.println(sb);
     }
-    public static void recur(int depth) {
+    public static void recur(int depth,int start) {
         if(depth==L) {
-            StringBuilder sb = new StringBuilder();
-            int mocount = 0;
-            int jacount = 0;
-            for (int i = 0; i < L; i++) {
-                if(answer[i]=='a'||answer[i]=='e'||answer[i]=='i'||answer[i]=='o'||answer[i]=='u')mocount++;
-                else jacount++;
-                temp[i] = answer[i];
-            }
             if(mocount>=1&&jacount>=2){
-                Arrays.sort(temp);
                 for (int i = 0; i < L; i++) {
-                    sb.append(temp[i]);
+                    sb.append(answer[i]);
                 }
-                hashSet.add(sb.toString());
+                sb.append("\n");
             }
             return;
         }
-        for (int i = depth; i < C; i++) {
-            if(visited[i]) continue;
+        for (int i = start; i < C; i++) {
+            //if(visited[i]) continue;
+
+            if(arr[i]=='a'||arr[i]=='e'||arr[i]=='i'||arr[i]=='o'||arr[i]=='u')mocount++;
+            else jacount++;
+            //visited[i] = true;
             answer[depth] = arr[i];
-            visited[i] = true;
-            recur(depth+1);
-            visited[i] = false;
+            recur(depth+1,i+1);
+            //visited[i] = false;
+            if(arr[i]=='a'||arr[i]=='e'||arr[i]=='i'||arr[i]=='o'||arr[i]=='u')mocount--;
+            else jacount--;
         }
     }
 }
