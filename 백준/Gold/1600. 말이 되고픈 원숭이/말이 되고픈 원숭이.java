ed60a2907c2fd4ab6908a75ab6478560a098c8af
sangmin0806 <134148399+sangmin0806@users.noreply.github.com>
2024-07-04 22:51:52 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 448 ms, Memory: 51928 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -5,86 +5,93 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
-    static int K,W,H;
-    static int[][] map;
-    static boolean[][][] visited;
-    static int dr[] = {0,0,1,-1};
-    static int dc[] = {1,-1,0,0};
-    static int hdr[] = {-1,-2,-2,-1,1,2,2,1};
-    static int hdc[] = {-2,-1,1,2,2,1,-1,-2};
-    static boolean flag = false;
-    static int answer =0;
+public class Main {
     static class Monkey{
         int row;
         int col;
-        int Kcount;
-        int totalcount;
+        int count;
 
-        public Monkey(int row, int col, int kcount, int totalcount) {
+        public Monkey(int row, int col, int count) {
             this.row = row;
             this.col = col;
-            Kcount = kcount;
-            this.totalcount = totalcount;
+            this.count = count;
         }
     }
+    static int K,W,H;
+    static boolean flag = false;
+    static int[][] map;
+    static int answer = 0;
+    static int[] dr = {1,0,-1,0};
+    static int[] dc = {0,1,0,-1};
+    static int[] hdr = {-2,-1,1,2,2,1,-1,-2};
+    static int[] hdc = {1,2,2,1,-1,-2,-2,-1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         K = Integer.parseInt(st.nextToken());
-        st= new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         W = Integer.parseInt(st.nextToken());
         H = Integer.parseInt(st.nextToken());
         map = new int[H][W];
-        visited = new boolean[H][W][K+1];
         for (int i = 0; i < H; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < W; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         bfs();
-        if(!flag){
-            System.out.println(-1);
-        }else {
+        if(flag){
             System.out.println(answer);
+        }
+        else{
+            System.out.println(-1);
         }
     }
     public static void bfs(){
         Queue<Monkey> que = new ArrayDeque<>();
-        que.offer(new Monkey(0,0,K,0));
-        visited[0][0][K] = true;
+        boolean[][][] visited = new boolean[K+1][H][W];
+        que.add(new Monkey(0,0,K));
+        visited[K][0][0] = true;
+
         while (!que.isEmpty()){
-            Monkey current = que.poll();
-            //System.out.println(current.row+" "+current.col+" "+ current.Kcount);
-            if(current.row==H-1&&current.col==W-1){
-                flag = true;
-                answer = current.totalcount;
-                return;
-            }
-            if(current.Kcount>0){
-                for (int i = 0; i < 8; i++) {
-                    int nrow = current.row+ hdr[i];
-                    int ncol = current.col +hdc[i];
-                    int ncount = current.Kcount-1;
-                    if(nrow<0||ncol<0||nrow>=H||ncol>=W) continue;
-                    if(visited[nrow][ncol][ncount]) continue;
-                    if(map[nrow][ncol]==1) continue;
-                    que.offer(new Monkey(nrow,ncol,ncount, current.totalcount+1));
-                    visited[nrow][ncol][ncount] = true;
+            int size = que.size();
+            answer++;
+            for (int q = 0; q < size; q++) {
+                Monkey current = que.poll();
+                if(current.row==H-1&&current.col==W-1){
+                    answer--;
+                    flag = true;
+                    return;
+                }
+                if(current.count>0){
+                    for (int i = 0; i < 8; i++) {
+                        int nrow = current.row+hdr[i];
+                        int ncol = current.col+hdc[i];
+                        int ncount = current.count-1;
+                        if(nrow<0||nrow>=H||ncol<0||ncol>=W)continue;
+                        if(visited[ncount][nrow][ncol]||map[nrow][ncol]==1)continue;
+
+                        visited[ncount][nrow][ncol] = true;
+                        que.add(new Monkey(nrow,ncol,ncount));
+
+                    }
+                }
+                for (int i = 0; i < 4; i++) {
+                    int nrow = current.row+dr[i];
+                    int ncol = current.col+dc[i];
+                    int count = current.count;
+                    if(nrow<0||nrow>=H||ncol<0||ncol>=W)continue;
+                    if(visited[count][nrow][ncol]||map[nrow][ncol]==1)continue;
+
+                    visited[count][nrow][ncol] = true;
+                    que.add(new Monkey(nrow,ncol,count));
                 }
             }
-            for (int i = 0; i < 4; i++) {
-                int nrow = current.row+ dr[i];
-                int ncol = current.col +dc[i];
-                if(nrow<0||ncol<0||nrow>=H||ncol>=W) continue;
-                if(visited[nrow][ncol][current.Kcount]) continue;
-                if(map[nrow][ncol]==1) continue;
-                que.offer(new Monkey(nrow,ncol,current.Kcount, current.totalcount+1));
-                visited[nrow][ncol][current.Kcount] = true;
-            }
+
         }
 
+
+
     }
+
 }
